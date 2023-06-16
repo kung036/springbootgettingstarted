d6fba7c544e27a49d32924b5e54ed8c7da71e3fa
kung036 <sinariari3@gmail.com>
2023-06-16 09:47:29 +0900

    fix: SpringBootApplication annotation not working

--- src/main/java/me/springbootgettingstarted/SpringbootgettingstartedApplication.java
+++ src/main/java/me/springbootgettingstarted/SpringbootgettingstartedApplication.java
@@ -9,5 +9,4 @@ public class SpringbootgettingstartedApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootgettingstartedApplication.class, args);
     }
-
 }
